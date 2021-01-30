f019ad1e4f56343ccb139418ab8d555e984e1aef
austin-owensby <austin_owensby@hotmail.com>
2021-01-29 18:56:48 -0600

    Use docs folder instead of wwwroot

--- Test/Program.cs
+++ Test/Program.cs
@@ -14,7 +14,7 @@ namespace Test
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseWebRoot("docs");
                 });
     }
 }
